57f4885fba1739c75884e3cce4bc30567b4d2f72
zebaameen2 <tayyabameen276@gmail.com>
2025-11-24 18:05:32 +0530

    add : something

---
+++ Module2/JS_Assesment_2/Ques2.js
@@ -0,0 +1,27 @@
+function processStudents(students) {
+  const above60 = students.filter(student => student.marks > 60);
+   const sortedStudents=above60 .sort((a, b) => b.marks - a.marks)
+    const names = sortedStudents.map(student => student.name);
+  return names
+
+
+  
+} 
+const students = [  { "name": "Alice", "marks": 58 },
+  { "name": "Bob", "marks": 85 },
+  { "name": "Charlie", "marks": 92 },
+  { "name": "David", "marks": 45 },
+  { "name": "Emma", "marks": 76 },
+  { "name": "Frank", "marks": 63 },
+  { "name": "Grace", "marks": 89 },
+  { "name": "Hannah", "marks": 95 },
+  { "name": "Ian", "marks": 54 },
+  { "name": "Jack", "marks": 79 },
+  { "name": "Kate", "marks": 67 },
+  { "name": "Leo", "marks": 88 },
+  { "name": "Mia", "marks": 91 },
+  { "name": "Nathan", "marks": 72 },
+  { "name": "Olivia", "marks": 82 }
+]
+
+console.log(processStudents(students));
