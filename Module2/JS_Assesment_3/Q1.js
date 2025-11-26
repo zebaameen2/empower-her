f1c2339bd60bea7d59a1d67ba7cd7f9e1a7f8ecd
zebaameen2 <tayyabameen276@gmail.com>
2025-11-26 10:16:08 +0530

    ADD: added something

---
+++ Module2/JS_Assesment_3/Q1.js
@@ -0,0 +1,11 @@
+var age=25
+function displayAge(){
+
+    console.log("Age is: "+age);
+}
+displayAge();
+function changeAge(){
+    age=30
+    console.log("Changed Age is: "+age);
+}
+changeAge();
---
+++ Module2/JS_Assesment_3/Q2.js
@@ -0,0 +1,14 @@
+function createCounter() {
+    let count = 0;
+    function counter() {
+        count += 1;
+        return count;
+    }
+    return counter;
+}       
+module.exports = createCounter;
+// Example usage:
+ const myCounter = createCounter();
+    console.log(myCounter()); // Output: 1  
+    console.log(myCounter()); // Output: 2
+console.log(myCounter()); // Output: 3
